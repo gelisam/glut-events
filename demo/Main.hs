3efb58bee7d982c31eb7c5273241c38ff9d4a756
Samuel Gélineau <gelisam@gmail.com>
2013-03-14 21:18:26 -0400

    Using a stroke font
    GLUT's bitmap fonts are strangely unresponsive to matrix
    transformations, making them unusable for displaying more than one line
    of text at a time. GLUT's stroke fonts suffer from the opposite problem:
    they require matrix scaling in order to display anything, because the
    default font size is much larger than the window size.

--- demo/Main.hs
+++ demo/Main.hs
@@ -8,10 +8,22 @@ import Graphics.UI.GLUT.Fonts
 scale_uniformly :: GLdouble -> IO ()
 scale_uniformly f = scale f f f
 
+renderText :: (GLdouble, GLdouble) -> GLdouble -> String -> IO ()
+renderText (x, y) h s = preservingMatrix $ do translate (Vector3 x y 0.0)
+                                              scale_uniformly (h / 100.0)
+                                              renderString Roman s
+
+-- the default GLUT window coordinates go from (-1, 1) at the top-left
+-- to (1, -1) at the bottom right, so this places the message towards
+-- the top left, with a font size of 5% of the window.
+displayMessage :: String -> IO ()
+displayMessage s = do clear [ColorBuffer]
+                      renderText (-0.8, 0.5) 0.1 s
+                      swapBuffers
+
+
 drawWindow :: IO ()
-drawWindow = do clear [ColorBuffer]
-                renderString Helvetica18 "Press ESC to quit."
-                swapBuffers
+drawWindow = displayMessage "Press ESC to quit."
 
 handleEvents :: Key -> KeyState -> Modifiers -> Position -> IO ()
 handleEvents (Char '\x1b' {-esc-}) _ _ _ = exitSuccess
