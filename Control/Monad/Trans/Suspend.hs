d1d8cb6a76515104d3f569af437913f39738b50c
Samuel Gélineau <gelisam+github@gmail.com>
2013-04-01 19:15:02 -0400

    The monad of Suspended computations
    A discovery: input events are clearly an example of interacting with the
    outside world, but surprisingly, waiting for the next event doesn't have
    to use the IO monad!
    
    The Suspend monad allows a pure computation to be halted while waiting
    for a pending event to occur. The idea is that this pure computation
    would be executed by an IO wrapper which resume suspended computations
    by sending it the actual events.

---
+++ Control/Monad/Trans/Suspend.hs
@@ -0,0 +1,16 @@
+module Control.Monad.Trans.Suspend where
+
+import Control.Monad.Trans
+
+
+data Suspend e a = Done a | Suspended (e -> Suspend e a)
+
+instance Functor (Suspend e) where
+  fmap f (Done x) = Done (f x)
+  fmap f (Suspended cc) = Suspended (fmap f . cc)
+
+instance Monad (Suspend e) where
+  return = Done
+  Done x >>= f = f x
+  Suspended cc >>= f = Suspended $ \e -> do x <- cc e
+                                            f x
